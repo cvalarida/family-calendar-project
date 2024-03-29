dcd7faf5b8299e4b4256476180efb7040dd842d6
Chris Valarida <cvalarida@gmail.com>
2024-03-28 21:40:58 -0700

    CalendarEvent type stub

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    interface PageData {
+      allCalendarEvents?: CalendarEvent[];
+    }
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
-export {};
+interface CalendarEvent {
+  // TODO FAM-12
+}
+
+export { };
